0d1e4eddd34f1f5eec54c54384a1e66f1a43b6f1
hasina821 <80751503+hasina821@users.noreply.github.com>
2021-11-06 14:00:10 +0000

    dynamisation-proj-slide

---
+++ libs/js/proj.js
@@ -0,0 +1,35 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+showSlides(slideIndex += n);
+}
+function ChangeSlide(){
+    setInterval(function() {plusSlides(slideIndex)},3000);
+
+}
+ChangeSlide();
+
+
+
+
+function currentSlide(n) {
+showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+var i;
+var slides = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("dot");
+if (n > slides.length) {slideIndex = 1}    
+if (n < 1) {slideIndex = slides.length}
+for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+}
+for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace("dot-active", "");
+}
+slides[slideIndex-1].style.display = "block";  
+dots[slideIndex-1].className += " dot-active";
+}
