7e565a04f55763b8e0b7a794450aaa4f663f6f4d
hasina821 <80751503+hasina821@users.noreply.github.com>
2021-11-06 14:01:40 +0000

    dynamisation-proj-slide

--- views/post.php
+++ views/post.php
@@ -488,37 +488,7 @@
     <script src="./libs/js/autotype.js"></script>
     <script src='./libs/js/aos.js'></script>
     <script src="./libs/js/scripts.js"></script>
-
-    
-    <script>
-        var slideIndex = 1;
-        showSlides(slideIndex);
-
-        function plusSlides(n) {
-        showSlides(slideIndex += n);
-        }
-
-        function currentSlide(n) {
-        showSlides(slideIndex = n);
-        }
-
-        function showSlides(n) {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        if (n > slides.length) {slideIndex = 1}    
-        if (n < 1) {slideIndex = slides.length}
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = "none";  
-        }
-        for (i = 0; i < dots.length; i++) {
-            dots[i].className = dots[i].className.replace("dot-active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " dot-active";
-        }
-    </script>
-
+    <script src="./libs/js/proj.js"></script>
 
 </body>
 </html>
